058a835f79e245147d010ba8896eb5511d08a5ff
Jônathas Assunção <jaa020399@gmail.com>
2021-06-11 01:03:39 -0300

    feat: Added attachment support

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  const bodyParser = require('body-parser');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(
+    bodyParser.urlencoded({
+      limit: '50mb',
+      extended: true,
+      parameterLimit: 50000,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
