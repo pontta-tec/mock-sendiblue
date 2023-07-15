fe7fb11dc63d4bab12d87588ff896fa403c0ec7f
Guilherme Pacheco <guilherme.pacheco@pontta.com>
2023-07-15 11:35:07 -0300

    Atualizacao de versao do NestJs e Node

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,29 @@
-FROM node:12.13-alpine AS builder
+FROM node:lts as builder
+# Create app directory
 WORKDIR /app
-COPY ./package.json ./
-RUN npm install
-COPY . .
-RUN npm run build
 
-FROM node:12.13-alpine
+# Install app dependencies
+COPY package.json yarn.lock ./
+
+RUN yarn install --frozen-lockfile
+
+COPY . .
+
+RUN yarn build
+
+FROM node:lts-slim
+
+ENV NODE_ENV production
+USER node
+
+# Create app directory
 WORKDIR /app
+
+# Install app dependencies
+COPY package.json yarn.lock ./
+
+RUN yarn install --production --frozen-lockfile
+
 COPY --from=builder /app ./
-CMD ["npm", "run", "start:prod"]
+
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useBodyParser('json', { limit: '50mb' });
   app.setViewEngine('hbs');
-
-  const bodyParser = require('body-parser');
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(
-    bodyParser.urlencoded({
-      limit: '50mb',
-      extended: true,
-      parameterLimit: 50000,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
